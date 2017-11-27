7dd8432bc09f985eec5e0c7178d180cbfce0a193
TaoYuan <1876665310@qq.com>
2017-11-27 16:48:49 +0800

    添加方法

--- app/src/main/cpp/MyJNI.cpp
+++ app/src/main/cpp/MyJNI.cpp
@@ -1,8 +1,8 @@
 #include <jni.h>
 #include <string>
 
-extern "C"
-JNIEXPORT jstring JNICALL
+extern "C"  //表示对java输出
+JNIEXPORT jstring JNICALL // 输入类型
 Java_com_test_ndkdemo_MyJNI_sayHello(JNIEnv *env, jobject instance) {
 
     std::string say = "say 就 say！\n Hello";
@@ -10,8 +10,24 @@ Java_com_test_ndkdemo_MyJNI_sayHello(JNIEnv *env, jobject instance) {
     return env->NewStringUTF(say.c_str());
 }
 
-extern "C" //表示对java输出
+extern "C"
 JNIEXPORT jint JNICALL
 Java_com_test_ndkdemo_MyJNI_sum(JNIEnv *env, jobject instance, jint x, jint y) {
     return x + y;
+}
+
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_test_ndkdemo_MyJNI_receiveYUV(JNIEnv *env, jobject instance, jbyteArray bytes_) {
+    jbyte *bytes = env->GetByteArrayElements(bytes_, NULL);
+    if (bytes == NULL) {
+        return 0; /* exception occurred */
+    }
+
+    //do something...
+    std::string say = "输出的json数据";
+    env->ReleaseByteArrayElements(bytes_, bytes, 0);
+
+    return env->NewStringUTF(say.c_str());
 }
